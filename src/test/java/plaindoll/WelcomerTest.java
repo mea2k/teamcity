d6efd631b4d9b8a451f67a09c0881f7a43d91203
mea2k <mea@2k>
2024-11-29 00:40:12 +0300

    add method sayName and tests

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -18,6 +18,10 @@ public class WelcomerTest {
 		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
 	}
 	@Test
+	public void welcomerSaysName() {
+		assertThat(welcomer.sayName(), containsString("Hunter"));
+	}
+	@Test
 	public void welcomerSaysHunter() {
 		assertThat(welcomer.sayWelcome(), containsString("hunter"));
 		assertThat(welcomer.sayFarewell(), containsString("hunter"));
